7ec5810dce056f2d8bb97c6fab252ff8f7f465c7
FikoReborn <fikosan@gmail.com>
2018-04-27 14:06:27 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,33 +1,36 @@
 $(document).ready(function() {
-  // Make navigation menu fixed if scrolled past header
 
-  $(window).scroll(function() {
-    var windowTop = $(window).scrollTop();
-    var headerHeight = $("header").height();
-    if (windowTop > headerHeight) {
-      $("nav").addClass("fixed-header");
-    } else {
-      $("nav").removeClass("fixed-header");
-    }
-  });
+    // Make navigation menu fixed if scrolled past header
+    $(window).scroll(function() {
+        var windowTop = $(window).scrollTop();
+        var headerHeight = $("header").height();
+        if (windowTop > headerHeight) {
+            $("nav").addClass("fixed-header");
+        }
+        else {
+            $("nav").removeClass("fixed-header");
+        }
+    });
 
-  // Pull out mobile menu when menu button is clicked
+    // Pull out mobile menu when menu button is clicked
+    $(".hamburger").click(function() {
+        $(".menu-list").toggleClass("active");
+    });
 
-  $(".hamburger").click(function() {
-    $(".menu-list").toggleClass("active");
-  });
+    // Close mobile menu when item is clicked
+    $(".menu-button").click(function() {
+        $(".menu-list").removeClass("active");
+    });
 
-  // Close mobile menu when item is clicked
+    // Make project image "hover" when text is hovered
+    $(".project-text").hover(function() {
+        $(this).prev().toggleClass("hoverImage");
+    });
 
-  $(".menu-button").click(function() {
-    $(".menu-list").removeClass("active");
-  });
+    // Clicking on project image should follow layer'd text's href
+    $(".project-image").click(function() {
+        var projectLink = $(this).next();
+        window.open(projectLink.attr("href"));
+    });
 
-  // Make text and image on projects section add "active" image class on hover
-
-  $(".project-text").hover(function() {
-    $(this)
-      .prev()
-      .toggleClass("hoverImage");
-  });
 });
